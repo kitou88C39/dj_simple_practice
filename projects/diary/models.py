a6d5413918d2a715f469e8b33a6f03fdbff5c466
kitou88C39 <kanfutrooper@icloud.com>
2024-01-30 11:14:17 +0900

    Edit models.py

--- projects/diary/models.py
+++ projects/diary/models.py
@@ -2,4 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class Day(models.Model):
-    title=models.CharField('タイトル',max_length=200)
+    title=models.CharField('タイトル', max_length=200)
+    text=models.TextField('本文')
+    date=models.DateTimeField('日付', default=timezone.now)
